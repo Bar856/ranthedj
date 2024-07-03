10b66e639f46835ca3d47414be3f3287467ce49c
Bar856 <barmaizel1@gmail.com>
2024-07-03 22:05:48 +0300

    css

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 import Image from "next/image";
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center p-24 text-center">
+    <main className="bg-black text-white flex min-h-screen flex-col items-center justify-center p-24 text-center">
       <Image src="/logo.png" alt="DJ Logo" width={350} height={350} />
       <h1 className="text-5xl font-bold">בקרוב.....</h1>
       <div className="mt-4">
@@ -10,16 +10,15 @@ export default function Home() {
           <a
             href="https://www.facebook.com/profile.php?id=100056682145355"
             target="_blank"
-            className="text-white"
             rel="noopener noreferrer">
-            <Image src="/facebook.svg" alt="Facebook" width={40} height={40} />
+            <Image src="/facebook.png" alt="Facebook" width={40} height={40} />
           </a>
           <a
             href="https://www.instagram.com/ran_the_dj/"
             target="_blank"
             rel="noopener noreferrer">
             <Image
-              src="/instagram.svg"
+              src="/instagram.png"
               alt="Instagram"
               width={40}
               height={40}
